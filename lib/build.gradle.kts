6af227c797a3f825189a52cfd8ff43850ff309f2
Alexander Sparkowsky <info@roamingthings.de>
2023-11-04 12:03:58 +0100

    ci: add workflows

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     `java-library`
+    `maven-publish`
     alias(libs.plugins.aspectj)
 }
+
+val mavenArtifactId = "lambda-circuit-breaker"
+val repositoryName = "roamingthings/lambda-circuit-breaker"
+val longName = "Circuit Breaker for AWS Lambda Functions"
+val longDescription = "A circuit breaker implementation intended for AWS Lambda functions, especially AWS Lambda functions running in\n" +
+        "a StepFunctions state machine.."
+
+group = "de.roamingthings"
 
 repositories {
     mavenCentral()
@@ -35,6 +44,45 @@ configurations.all {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+        vendor = JvmVendorSpec.AMAZON
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${repositoryName}")
+            credentials {
+                username = "dummy"
+                password = project.findProperty("gpr.key") as String? ?: (System.getenv("GITHUB_TOKEN"))
+                        ?: System.getenv("GITHUB_PUBLISH_PACKAGES_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("library") {
+            groupId = project.group.toString()
+            artifactId = mavenArtifactId
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name.set(longName)
+                description.set(longDescription)
+                url.set("https://github.com/${repositoryName}")
+
+                scm {
+                    connection.set("https://github.com/${repositoryName}.git")
+                    developerConnection.set("https://github.com/${repositoryName}.git")
+                    url.set("https://github.com/${repositoryName}")
+                }
+            }
+        }
+    }
 }
